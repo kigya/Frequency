1ada02df0e8d06086cc4649882fa9b93b11e73f7
Vladimir Lari4ev <freeky92s@gmail.com>
2022-03-08 01:50:10 +0300

    07.03.2022
    add profile fragment and vm

---
+++ app/src/main/java/com/example/frequency/screen/profile/ProfileVM.kt
@@ -0,0 +1,30 @@
+package com.example.frequency.screen.profile
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.SavedStateHandle
+import com.example.frequency.foundation.views.BaseVM
+import com.example.frequency.model.User
+import com.example.frequency.preferences.AppDefaultPreferences
+import com.example.frequency.utils.share
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+const val KEY_USER = "KEY_USER"
+
+@HiltViewModel
+class ProfileVM @Inject constructor(
+    shearedPreferences: AppDefaultPreferences,
+    savedStateHandle: SavedStateHandle
+) : BaseVM() {
+
+    private val userFormPref = User(
+        shearedPreferences.getUsername(),
+        shearedPreferences.getEmail(),
+        shearedPreferences.getIconUri(),
+        shearedPreferences.getToken(),
+        )
+
+    private val _userLD = savedStateHandle.getLiveData<User>(KEY_USER, userFormPref)
+    val user: LiveData<User> = _userLD.share()
+
+}
