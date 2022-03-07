2590d2b585784b31d777f9e0c06f1f406bbbb76b
Vladimir Lari4ev <freeky92s@gmail.com>
2022-03-08 01:54:31 +0300

    07.03.2022
    add abstract class Action may not have a runnable

--- app/src/main/java/com/example/frequency/foundation/model/Action.kt
+++ app/src/main/java/com/example/frequency/foundation/model/Action.kt
@@ -3,5 +3,5 @@ package com.example.frequency.foundation.model
 abstract class Action {
     abstract val iconRes: Int
     abstract val textRes: Int
-    abstract val onCustomAction: Runnable
+    abstract val onCustomAction: Runnable?
 }
