e59b50f1cb9dca174bbc79adaa7ab6ec162c4219
Vladimir Lari4ev <freeky92s@gmail.com>
2022-03-08 01:41:05 +0300

    07.03.2022
    add class User

---
+++ app/src/main/java/com/example/frequency/model/User.kt
@@ -0,0 +1,13 @@
+package com.example.frequency.model
+
+import android.net.Uri
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class User(
+    val name: String,
+    val email: String,
+    val icon: Uri,
+    val gToken: String
+): Parcelable
