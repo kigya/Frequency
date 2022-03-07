1830dc9a9237f695c02b6e4570cdfbdeeb8dd5e7
kirillborichevskiy <kirilborichevskiy@gmail.com>
2022-03-07 13:42:14 +0300

    Add splash beta

--- app/src/main/java/com/example/frequency/MainActivity.kt
+++ app/src/main/java/com/example/frequency/MainActivity.kt
@@ -2,15 +2,23 @@ package com.example.frequency
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.frequency.databinding.ActivityMainBinding
 import com.example.frequency.databinding.ActivityMainBinding.inflate
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                viewModel.isLoading.value
+            }
+        }
         binding = inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
