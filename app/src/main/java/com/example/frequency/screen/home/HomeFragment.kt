a3b2e5458ea31029f9e94ab9e9e744e8f3876ef2
Vladimir Lari4ev <freeky92s@gmail.com>
2022-03-07 18:08:10 +0300

    07.03.2022
    remove list layout
    
    edit fragment home
    
    replace <TV> to -
    getString(Res, StringValue)(composite)
    
    refactor string resource

--- app/src/main/java/com/example/frequency/screen/home/HomeFragment.kt
+++ app/src/main/java/com/example/frequency/screen/home/HomeFragment.kt
@@ -37,6 +37,8 @@ class HomeFragment : BaseFragment(), ProvidesCustomTitle, ProvidesCustomActions 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         // initialise listeners
 
+        binding.helloTextview.text = getString(R.string.hello_username, getString(R.string.user))
+
         return binding.root
     }
 
