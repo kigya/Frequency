2fbc03ab490d20b4d4d0876e7703e15063625017
Vladimir Larichev <freeky92s@gmail.com>
2022-03-09 19:10:09 +0300

    09.03.2022
    add slide animation

--- app/src/main/java/com/example/frequency/screen/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/frequency/screen/profile/ProfileFragment.kt
@@ -1,10 +1,17 @@
 package com.example.frequency.screen.profile
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
+import com.example.frequency.MainActivity
 import com.example.frequency.R
 import com.example.frequency.databinding.FragmentProfileBinding
 import com.example.frequency.foundation.contract.ProvidesCustomActions
@@ -12,7 +19,8 @@ import com.example.frequency.foundation.contract.ProvidesCustomTitle
 import com.example.frequency.foundation.contract.navigator
 import com.example.frequency.foundation.views.BaseFragment
 import com.example.frequency.screen.song.SongFragment
-import com.example.frequency.utils.menuAction
+import com.example.frequency.utils.ActionStore.menuAction
+import com.example.frequency.utils.setUserImageByGlide
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -36,21 +44,44 @@ class ProfileFragment : BaseFragment(), ProvidesCustomActions, ProvidesCustomTit
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         // initialise listeners
 
-        binding.userIcon.setOnClickListener {
+        binding.includedProfileInfo.userIcon.setOnClickListener {
             navigator().openSettings()
         }
 
         viewModel.user.observe(viewLifecycleOwner) {
             if (it == null) return@observe
-            binding.userNameTv.text = it.name
-            binding.userEmailTv.text = it.email
-            binding.userIcon.setImageURI(it.icon)
+            binding.includedProfileInfo.userNameTv.text = it.name
+            binding.includedProfileInfo.userEmailTv.text = it.email
+            setUserImageByGlide(requireContext(), binding.includedProfileInfo.userIcon, it.icon)
+            binding.includedProfileInfo.central.isVisible = true
         }
+
+        setListeners()
 
         return binding.root
     }
 
-    override fun getTitleRes() = R.string.profile
+    private fun setListeners(){
+        binding.logoutTb.setOnClickListener {
+            viewModel.clearUserRootPreferences()
+            Handler(Looper.getMainLooper()).postDelayed(
+                {
+                    triggerRebirth(requireContext())
+                }, 1000
+            )
+        }
+
+    }
+
+    private fun triggerRebirth(context: Context) {
+        val intent = Intent(requireContext(), MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        context.startActivity(intent)
+        if (context is Activity) {
+            context.finish()
+        }
+        Runtime.getRuntime().exit(0)
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
@@ -65,6 +96,8 @@ class ProfileFragment : BaseFragment(), ProvidesCustomActions, ProvidesCustomTit
             }
         }
     }
+
+    override fun getTitleRes() = R.string.profile
 
     override fun getCustomActions() = listOf(menuAction)
 }
