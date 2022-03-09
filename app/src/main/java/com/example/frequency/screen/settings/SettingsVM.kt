176207baf998fe56ec2f0e0962cb5f90e5d24f9c
Vladimir Larichev <freeky92s@gmail.com>
2022-03-09 19:27:00 +0300

    09.03.2022
    add di injection email LD

--- app/src/main/java/com/example/frequency/screen/settings/SettingsVM.kt
+++ app/src/main/java/com/example/frequency/screen/settings/SettingsVM.kt
@@ -1,7 +1,23 @@
 package com.example.frequency.screen.settings
 
+import androidx.lifecycle.SavedStateHandle
 import com.example.frequency.foundation.views.BaseVM
+import com.example.frequency.preferences.AppDefaultPreferences
+import com.example.frequency.utils.share
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class SettingsVM : BaseVM() {
+@HiltViewModel
+class SettingsVM @Inject constructor(
+    shearedPreferences: AppDefaultPreferences,
+    savedStateHandle: SavedStateHandle
+) : BaseVM() {
 
+    private val _usersEmailLD = savedStateHandle.getLiveData(STATE_KEY_EMAIL, shearedPreferences.getEmail())
+    val usersEmailLD = _usersEmailLD.share()
+
+    companion object {
+        @JvmStatic
+        private val STATE_KEY_EMAIL = "STATE_KEY_EMAIL"
+    }
 }
