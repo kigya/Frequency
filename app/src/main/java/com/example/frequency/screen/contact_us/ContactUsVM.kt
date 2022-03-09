9c586763b718b61fbd7af954ee369796e198f6f4
Vladimir Larichev <freeky92s@gmail.com>
2022-03-09 19:20:08 +0300

    09.03.2022
    add contact us

---
+++ app/src/main/java/com/example/frequency/screen/contact_us/ContactUsVM.kt
@@ -0,0 +1,10 @@
+package com.example.frequency.screen.contact_us
+
+import com.example.frequency.foundation.views.BaseVM
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ContactUsVM @Inject constructor(): BaseVM() {
+
+}
