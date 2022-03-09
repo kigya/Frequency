509bd45622858ec3224725a38fdcc81d05470d9b
Vladimir Larichev <freeky92s@gmail.com>
2022-03-09 19:27:34 +0300

    09.03.2022
    add firebase and google auth logic

--- app/src/main/java/com/example/frequency/screen/sign_in/SignInFragment.kt
+++ app/src/main/java/com/example/frequency/screen/sign_in/SignInFragment.kt
@@ -1,19 +1,43 @@
 package com.example.frequency.screen.sign_in
 
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import com.example.frequency.R
 import com.example.frequency.databinding.FragmentSignInBinding
-import com.example.frequency.foundation.contract.ProvidesCustomActions
 import com.example.frequency.foundation.contract.ProvidesCustomTitle
+import com.example.frequency.foundation.contract.navigator
 import com.example.frequency.foundation.views.BaseFragment
+import com.example.frequency.screen.home.HomeFragment
 import com.example.frequency.screen.song.SongFragment
-import com.example.frequency.utils.menuAction
+import com.example.frequency.utils.setUserImageByGlide
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import dagger.hilt.android.AndroidEntryPoint
 
-class SignInFragment : BaseFragment(), ProvidesCustomActions, ProvidesCustomTitle {
+@AndroidEntryPoint
+class SignInFragment : BaseFragment(), ProvidesCustomTitle {
+
+    private val launchGoogleRegister =
+        registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult(),
+            ::onUserDataReceived
+        )
+    private lateinit var auth: FirebaseAuth
 
     override val viewModel by viewModels<SignInVM>()
 
@@ -23,6 +47,9 @@ class SignInFragment : BaseFragment(), ProvidesCustomActions, ProvidesCustomTitl
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // initialise all data
+
+        auth = Firebase.auth
+
     }
 
     override fun onCreateView(
@@ -33,9 +60,75 @@ class SignInFragment : BaseFragment(), ProvidesCustomActions, ProvidesCustomTitl
         _binding = FragmentSignInBinding.inflate(inflater, container, false)
         // initialise listeners
 
+        binding.gogSignIn.setOnClickListener {
+            launchGoogleRegister.launch(getClient().signInIntent)
+        }
+
+        initiateObservers()
         return binding.root
     }
 
+    private fun initiateObservers() {
+
+        viewModel.registerUserLD.observe(viewLifecycleOwner) {
+            provideDataFirebase(it.gToken)
+            binding.includedProfileInfo.userNameTv.text = it.name
+            setUserImageByGlide(requireContext(), binding.includedProfileInfo.userIcon, it.icon)
+            binding.includedProfileInfo.central.isVisible = true
+        }
+
+
+    }
+
+    private fun onUserDataReceived(result: ActivityResult) {
+        val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+        try {
+            val account = task.getResult(ApiException::class.java)
+            if (account != null) {
+                viewModel.registerUser(
+                    account.displayName.toString(),
+                    account.email.toString(),
+                    account.photoUrl ?: Uri.EMPTY,
+                    account.idToken.toString(),
+                )
+            } else {
+                Log.d(TAG, "account == null")
+            }
+        } catch (e: ApiException) {
+            showSnackbar("Error ${e.message.toString()} ApiException")
+        }
+
+    }
+
+    private fun getClient(): GoogleSignInClient {
+        val gso = GoogleSignInOptions
+            .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestProfile()
+            .requestEmail()
+            .build()
+        return GoogleSignIn.getClient(requireActivity(), gso)
+    }
+
+    private fun provideDataFirebase(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential).addOnCompleteListener {
+            if (it.isSuccessful) {
+                showSnackbar(getString(R.string.auth_success))
+                navigator().openFragment(HomeFragment(), clearBackstack = true, addToBackStack = false)
+            } else {
+                showSnackbar(getString(R.string.auth_fail))
+            }
+        }
+    }
+
+    private fun showSnackbar(message: String) {
+        val snackBar = Snackbar.make(binding.root, message, Snackbar.LENGTH_SHORT)
+        snackBar.view.setOnClickListener {
+            snackBar.dismiss()
+        }
+        snackBar.show()
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
@@ -43,6 +136,9 @@ class SignInFragment : BaseFragment(), ProvidesCustomActions, ProvidesCustomTitl
     }
 
     companion object {
+
+        private val TAG = SignInFragment::class.java.simpleName
+
         @JvmStatic
         fun newInstance() = SongFragment().apply {
             arguments = Bundle().apply {
@@ -52,7 +148,5 @@ class SignInFragment : BaseFragment(), ProvidesCustomActions, ProvidesCustomTitl
     }
 
     override fun getTitleRes() = R.string.sign_in
-
-    override fun getCustomActions() = listOf(menuAction)
 
 }
