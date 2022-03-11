6e3c977f0c5e846024a5259afb1b019acff235db
Vladimir Larichev <freeky92s@gmail.com>
2022-03-11 15:36:16 +0300

    09.03.2022
    change second param

---
+++ app/src/main/java/com/example/frequency/model/SnackBarEntity.kt
@@ -0,0 +1,12 @@
+package com.example.frequency.model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class SnackBarEntity(
+    val message: Int,
+    val iconTag: Int
+) : Parcelable {
+
+}
