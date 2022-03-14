4417c05cbd70566969f28dfa5c45b1c6a9683d20
Vladimir Larichev <freeky92s@gmail.com>
2022-03-14 13:59:42 +0300

    14.03.2022
    fix permissions

--- app/src/main/java/com/example/frequency/screen/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/frequency/screen/profile/ProfileFragment.kt
@@ -11,8 +11,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.activity.result.contract.ActivityResultContracts.OpenDocument
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts.*
 import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import com.bumptech.glide.Glide
@@ -27,6 +27,7 @@ import com.example.frequency.foundation.views.BaseFragment
 import com.example.frequency.utils.ActionStore.menuAction
 import com.example.frequency.utils.SummaryUtils.ALERT
 import com.example.frequency.utils.SummaryUtils.ERROR
+import com.example.frequency.utils.SummaryUtils.SUCCESS
 import com.example.frequency.utils.SummaryUtils.showSnackbar
 import com.example.frequency.utils.UtilPermission.ALL_PERMISSIONS
 import com.example.frequency.utils.UtilPermission.hasPermissions
@@ -37,6 +38,7 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class ProfileFragment : BaseFragment(), ProvidesCustomActions, ProvidesCustomTitle {
 
+
     override val viewModel by viewModels<ProfileVM>()
 
     private var _binding: FragmentProfileBinding? = null
@@ -44,53 +46,16 @@ class ProfileFragment : BaseFragment(), ProvidesCustomActions, ProvidesCustomTit
 
     private val currentUser get() = viewModel.user.value
 
+    private val settingsLauncher =
+        registerForActivityResult(StartActivityForResult(), ::onSettingsResult)
+
     private val permissionLauncher = registerForActivityResult(
-        ActivityResultContracts.RequestMultiplePermissions(),
+        RequestMultiplePermissions(),
         ::onGotPermissionResult
     )
-
     private val pickImageLauncher = registerForActivityResult(OpenDocument(), ::onReferenceReceived)
 
-    private fun onReferenceReceived(uri: Uri?) {
-        uri?.let {
-            context?.contentResolver?.takePersistableUriPermission(
-                uri,
-                Intent.FLAG_GRANT_READ_URI_PERMISSION
-            )
-            viewModel.setUpdatedImage(uri)
-        }
-    }
-
-    private fun onGotPermissionResult(results: Map<String, Boolean>) {
-        if (hasPermissions(requireContext(), *results.keys.toTypedArray())) {
-            pickImageLauncher.launch(arrayOf("image/*"))
-        } else { //false
-            if (!shouldShowRequestPermissionRationale(results.keys.last())) {
-                askForOpeningSettings()
-            } else {
-                showSnackbar(binding.root, "Permission needed!", iconPreset = ALERT)
-            }
-        }
-    }
-
-    private val startSettingActivityIntent = Intent(
-        Settings.ACTION_APPLICATION_DETAILS_SETTINGS
-    )
-
-    private fun askForOpeningSettings() {
-        if (context?.packageManager?.resolveActivity(
-                startSettingActivityIntent, PackageManager.MATCH_DEFAULT_ONLY
-            ) == null
-        ) {
-            showSnackbar(binding.root, "Permission denied forever!", iconPreset = ERROR)
-        } else {
-            SimpleDialogFragment.show(
-                parentFragmentManager,
-                "Application work incorrectly.",
-                "Our app will not works without this permission, you can add it in settings."
-            )
-        }
-    }
+    private lateinit var startSettingActivityIntent: Intent
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -139,6 +104,58 @@ class ProfileFragment : BaseFragment(), ProvidesCustomActions, ProvidesCustomTit
 
     }
 
+    private fun onReferenceReceived(uri: Uri?) {
+        uri?.let {
+            context?.contentResolver?.takePersistableUriPermission(
+                uri,
+                Intent.FLAG_GRANT_READ_URI_PERMISSION
+            )
+            viewModel.setUpdatedImage(uri)
+        }
+    }
+
+    private fun onGotPermissionResult(results: Map<String, Boolean>) {
+        if (hasPermissions(requireContext(), *results.keys.toTypedArray())) {
+            pickImageLauncher.launch(arrayOf("image/*"))
+        } else { //false
+            if (!shouldShowRequestPermissionRationale(results.keys.last())) {
+                askForOpeningSettings()
+            } else {
+                showSnackbar(binding.root, "Permission needed!", iconPreset = ALERT)
+            }
+        }
+    }
+
+    private fun onSettingsResult(result: ActivityResult) {
+        permissionLauncher.launch(ALL_PERMISSIONS)
+    }
+
+    private fun setSimpleDialogFragmentListener() {
+        SimpleDialogFragment.setUpListener(parentFragmentManager, viewLifecycleOwner) {
+            settingsLauncher.launch(startSettingActivityIntent)
+        }
+    }
+
+    private fun askForOpeningSettings() {
+        startSettingActivityIntent = Intent(
+            Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+            Uri.fromParts("package", context?.packageName, null)
+        )
+
+        if (context?.packageManager?.resolveActivity(
+                startSettingActivityIntent, PackageManager.MATCH_DEFAULT_ONLY
+            ) == null
+        ) {
+            showSnackbar(binding.root, "Permission denied forever!", iconPreset = ERROR)
+        } else {
+            SimpleDialogFragment.show(
+                parentFragmentManager,
+                "Application work incorrectly.",
+                "Our app will not works without this permission, you can add it in settings."
+            )
+        }
+    }
+
     private fun initiateObserver() {
         viewModel.user.observe(viewLifecycleOwner) {
             if (it == null) return@observe
@@ -157,19 +174,18 @@ class ProfileFragment : BaseFragment(), ProvidesCustomActions, ProvidesCustomTit
 
     }
 
-    private fun setImageByGlide(requireContext: Context, view: ImageView, uri: Uri?, timeout: Int = 0) {
+    private fun setImageByGlide(
+        requireContext: Context,
+        view: ImageView,
+        uri: Uri?,
+        timeout: Int = 0
+    ) {
         Glide.with(requireContext)
             .load(uri)
             .timeout(timeout)
             .error(R.drawable.ic_unknown_user_photo)
             .diskCacheStrategy(DiskCacheStrategy.ALL)
             .into(view)
-    }
-
-    private fun setSimpleDialogFragmentListener() {
-        SimpleDialogFragment.setUpListener(parentFragmentManager, viewLifecycleOwner) {
-            startSettingActivityIntent
-        }
     }
 
     private fun triggerRebirth(context: Context) {
