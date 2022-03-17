fd9122f8a1ce28e43d3987dfbd6979efd4cc5b55
Vladimir Larichev <freeky92s@gmail.com>
2022-03-17 09:37:58 +0300

    17.03.2022
    new listeners added

--- app/src/main/java/com/example/frequency/screen/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/frequency/screen/settings/SettingsFragment.kt
@@ -1,5 +1,8 @@
 package com.example.frequency.screen.settings
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +11,7 @@ import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import com.example.frequency.MainActivity
 import com.example.frequency.R
 import com.example.frequency.foundation.contract.ProvidesCustomActions
 import com.example.frequency.foundation.contract.ProvidesCustomTitle
@@ -16,6 +20,8 @@ import com.example.frequency.utils.ActionStore.menuAction
 import com.example.frequency.utils.ActionStore.provideProfileAction
 import com.example.frequency.utils.PreferenceTags.EMAIL
 import com.example.frequency.utils.PreferenceTags.USERNAME
+import com.example.frequency.utils.dialog_fragment.SimpleDialogFragment
+import com.example.frequency.utils.observeEvent
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -48,19 +54,64 @@ class SettingsFragment : PreferenceFragmentCompat(), ProvidesCustomTitle, Provid
             )
         )
 
+        val emailPref = findPreference<Preference>(EMAIL)
+        emailPref?.summary = viewModel.usersEmailLD.value
+
+
+        initiateListeners()
+        initiateObservers()
+        setSimpleDialogFragmentListener()
+    }
+
+    private fun initiateListeners() {
         findPreference<Preference>(USERNAME)?.setOnPreferenceChangeListener { _, newValue ->
             val user = currentUser
             navigator().provideResult(user.copy(name = newValue as String))
             return@setOnPreferenceChangeListener true
         }
-
-        val emailPref = findPreference<Preference>(EMAIL)
-        emailPref?.summary = viewModel.usersEmailLD.value
+        val logout = findPreference<Preference>(LOGOUT)
+        logout?.setOnPreferenceClickListener {
+            SimpleDialogFragment.show(
+                parentFragmentManager,
+                getString(R.string.logout_q),
+                getString(R.string.sure_q),
+                "Logout", "Close"
+            )
+            return@setOnPreferenceClickListener true
+        }
     }
+
+    private fun initiateObservers() {
+        viewModel.launchReset.observeEvent(viewLifecycleOwner) {
+            triggerRebirth(requireContext())
+        }
+        viewModel.showPbLd.observeEvent(viewLifecycleOwner){
+            navigator().showProgress(it)
+        }
+    }
+
+    private fun setSimpleDialogFragmentListener() {
+        SimpleDialogFragment.setUpListener(parentFragmentManager, viewLifecycleOwner) {
+            viewModel.showPB(true)
+            viewModel.clearUserRootPreferences()
+        }
+    }
+
+    private fun triggerRebirth(context: Context) {
+        val intent = Intent(requireContext(), MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        context.startActivity(intent)
+        if (context is Activity) {
+            context.finish()
+        }
+        Runtime.getRuntime().exit(0)
+        viewModel.showPB(false)
+    }
+
 
     override fun getTitleRes() = R.string.settings
 
-    override fun getCustomActions() = listOf(menuAction, provideProfileAction(){
+    override fun getCustomActions() = listOf(menuAction, provideProfileAction() {
         navigator().openProfile()
     })
 
@@ -71,6 +122,10 @@ class SettingsFragment : PreferenceFragmentCompat(), ProvidesCustomTitle, Provid
 
             }
         }
+
+        @JvmStatic
+        private val LOGOUT = "LOGOUT"
+
     }
 
 }
