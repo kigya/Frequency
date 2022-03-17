f4f99750a069fdc71b5cedb5543179464fc90ae3
Vladimir Larichev <freeky92s@gmail.com>
2022-03-17 09:38:24 +0300

    17.03.2022
    remove logout

--- app/src/main/java/com/example/frequency/screen/info/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/frequency/screen/info/profile/ProfileFragment.kt
@@ -1,6 +1,5 @@
-package com.example.frequency.screen.profile
+package com.example.frequency.screen.info.profile
 
-import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -17,7 +16,6 @@ import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
-import com.example.frequency.MainActivity
 import com.example.frequency.R
 import com.example.frequency.databinding.FragmentProfileBinding
 import com.example.frequency.foundation.contract.ProvidesCustomActions
@@ -27,7 +25,6 @@ import com.example.frequency.foundation.views.BaseFragment
 import com.example.frequency.utils.ActionStore.menuAction
 import com.example.frequency.utils.SummaryUtils.ALERT
 import com.example.frequency.utils.SummaryUtils.ERROR
-import com.example.frequency.utils.SummaryUtils.SUCCESS
 import com.example.frequency.utils.SummaryUtils.showSnackbar
 import com.example.frequency.utils.UtilPermission.ALL_PERMISSIONS
 import com.example.frequency.utils.UtilPermission.hasPermissions
@@ -78,9 +75,6 @@ class ProfileFragment : BaseFragment(), ProvidesCustomActions, ProvidesCustomTit
 
     private fun initiateListener() {
         with(binding) {
-            logoutTb.setOnClickListener {
-                viewModel.clearUserRootPreferences()
-            }
             includedProfileInfo.userIcon.setOnClickListener {
                 val user = currentUser
                 user?.let {
@@ -100,6 +94,21 @@ class ProfileFragment : BaseFragment(), ProvidesCustomActions, ProvidesCustomTit
                 return@setOnLongClickListener true
             }
 
+        }
+
+    }
+
+    private fun initiateObserver() {
+        viewModel.user.observe(viewLifecycleOwner) {
+            if (it == null) return@observe
+            binding.includedProfileInfo.userNameTv.text = it.name
+            binding.includedProfileInfo.userEmailTv.text = it.email
+            setImageByGlide(requireContext(), binding.includedProfileInfo.userIcon, it.icon, 100)
+            binding.includedProfileInfo.central.isVisible = true
+            navigator().provideResult(it)
+        }
+        viewModel.showPbLd.observeEvent(viewLifecycleOwner) {
+            navigator().showProgress(it)
         }
 
     }
@@ -150,28 +159,11 @@ class ProfileFragment : BaseFragment(), ProvidesCustomActions, ProvidesCustomTit
         } else {
             SimpleDialogFragment.show(
                 parentFragmentManager,
-                "Application work incorrectly.",
-                "Our app will not works without this permission, you can add it in settings."
+                getString(R.string.incorrect_app_work),
+                getString(R.string.works_without_perm),
+                "Settings", "Close"
             )
         }
-    }
-
-    private fun initiateObserver() {
-        viewModel.user.observe(viewLifecycleOwner) {
-            if (it == null) return@observe
-            binding.includedProfileInfo.userNameTv.text = it.name
-            binding.includedProfileInfo.userEmailTv.text = it.email
-            setImageByGlide(requireContext(), binding.includedProfileInfo.userIcon, it.icon, 100)
-            binding.includedProfileInfo.central.isVisible = true
-            navigator().provideResult(it)
-        }
-        viewModel.launchReset.observeEvent(viewLifecycleOwner) {
-            triggerRebirth(requireContext())
-        }
-        viewModel.showPbLd.observeEvent(viewLifecycleOwner) {
-            navigator().showProgress(it)
-        }
-
     }
 
     private fun setImageByGlide(
@@ -186,16 +178,6 @@ class ProfileFragment : BaseFragment(), ProvidesCustomActions, ProvidesCustomTit
             .error(R.drawable.ic_unknown_user_photo)
             .diskCacheStrategy(DiskCacheStrategy.ALL)
             .into(view)
-    }
-
-    private fun triggerRebirth(context: Context) {
-        val intent = Intent(requireContext(), MainActivity::class.java)
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-        context.startActivity(intent)
-        if (context is Activity) {
-            context.finish()
-        }
-        Runtime.getRuntime().exit(0)
     }
 
     override fun onDestroyView() {
